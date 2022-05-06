7f703821f6f6eaf9a2e3b8f9ff481383c9971b87
森史憲 <mori@codingdesing.jp>
2022-05-06 11:26:37 +0900

    お知らせ関連の定数を追加・調整

--- plugins/constants/info.js
+++ plugins/constants/info.js
@@ -1,6 +1,27 @@
 export default {
-  'LIST_LIMIT': 5,
-  'MAINTENANCE_LIST_LIMIT': 5,
-  'TROUBLE_LIST_LIMIT': 5,
+  'TOP_IMPORTANT_LIST_LIMIT': 3,
+  'TOP_RECRUIT_LIST_LIMIT': 3,
+  'TOP_INFO_LIST_LIMIT': 5,
+  'LIST_LIMIT': 20,
   'MONTH_LIST_LIMIT': 100,
+  'INFO_CATEGORIES': [
+    {
+      id: 1,
+      name: '通常',
+      text: 'お知らせ',
+      slug: 'normal',
+    },
+    {
+      id: 2,
+      name: '重要',
+      text: '重要なお知らせ',
+      slug: 'important',
+    },
+    {
+      id: 3,
+      name: '採用',
+      text: '採用',
+      slug: 'recruit',
+    },
+  ],
 }
